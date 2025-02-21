ef2a6179c9bee6b8625f47777109841998fbe209
victor47n <victor47n@gmail.com>
2025-02-21 08:07:08 -0300

    feat: add product header component

---
+++ src/app/[slug]/menu/[productId]/_components/product-header.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import type { Product } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/_components/ui/button";
+
+interface ProductHeaderProps {
+  product: Pick<Product, "name" | "imageUrl">;
+}
+
+export default function ProductHeader({ product }: ProductHeaderProps) {
+  const { back } = useRouter();
+  const handleBackClick = () => back();
+
+  return (
+    <div className="relative h-[300px] w-full">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-cover"
+      />
+
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 rounded-full"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
+
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 rounded-full"
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
+  );
+}
--- src/app/[slug]/menu/_components/products.tsx
+++ src/app/[slug]/menu/_components/products.tsx
@@ -1,6 +1,7 @@
 import type { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 import { formatCurrency } from "@/_utils/formatCurrency";
 
@@ -9,21 +10,36 @@ interface ProductsProps {
 }
 
 export default function Products({ products }: ProductsProps) {
+  const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className="space-y-3 px-5 py-3">
       {products.map((product) => (
-        <Link key={product.id} href="/" className="flex items-center justify-between gap-10 py-3 border-b">
+        <Link
+          key={product.id}
+          href={`/${slug}/menu/${product.id}`}
+          className="flex items-center justify-between gap-10 border-b py-3"
+        >
           <div>
             <h3 className="text-sm font-medium">{product.name}</h3>
-            <p className="line-clamp-2 text-sm text-muted-foreground">{product.description}</p>
-            <p className="pt-3 text-sm font-semibold">{formatCurrency(product.price)}</p>
+            <p className="line-clamp-2 text-sm text-muted-foreground">
+              {product.description}
+            </p>
+            <p className="pt-3 text-sm font-semibold">
+              {formatCurrency(product.price)}
+            </p>
           </div>
 
           <div className="relative min-h-[82px] min-w-[120px]">
-            <Image src={product.imageUrl} alt={product.name} fill className="rounded-lg object-contain" />
+            <Image
+              src={product.imageUrl}
+              alt={product.name}
+              fill
+              className="rounded-lg object-contain"
+            />
           </div>
         </Link>
       ))}
     </div>
-  )
+  );
 }
